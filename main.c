3c592dac443e07ee1e90a61d50f39754b6202a15
Quadro-Coders <186919462+Quadro-Coders@users.noreply.github.com>
2026-02-18 12:16:49 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,76 +1,102 @@
 #include "matrix2d.h"
 
-// Главное меню программы
 int main()
 {
-    matrix2d *matrix_a = NULL;  // матрица A
-    matrix2d *matrix_b = NULL;  // матрица B
+    matrix2d *matrix_a = NULL;
+    matrix2d *matrix_b = NULL;
     char choice;
 
-    printf("ПРАКТИЧЕСКАЯ РАБОТА №1\n");
-    printf("Двумерная матрица (matrix2d)\n");
+    printf("ПРАКТИЧЕСКАЯ РАБОТА №1 (Вариант 2)\n");
+    printf("Структура: Двумерная матрица (matrix2d)\n");
 
     do
     {
-        // Меню
         printf("\nМЕНЮ:\n");
         printf("a. Создать матрицу A\n");
         printf("b. Вывести матрицу A\n");
-        printf("c. Заполнить A случайно\n");
-        printf("d. Изменить элемент A\n");
-        printf("e. Инкремент A (++)\n");
-        printf("f. Декремент A (--)\n");
-        printf("g. Получить строку A\n");
-        printf("h. Получить столбец A\n");
-        printf("i. Транспонировать A\n");
-        printf("j. Определитель A\n");
+        printf("c. Заполнить A случайными значениями\n");
+        printf("d. Изменить элемент матрицы A\n");
+        printf("e. Инкремент матрицы A (++)\n");
+        printf("f. Декремент матрицы A (--)\n");
+        printf("g. Получить строку матрицы A\n");
+        printf("h. Получить столбец матрицы A\n");
+        printf("i. Транспонировать матрицу A\n");
+        printf("j. Определитель матрицы A\n");
         printf("k. Обратная матрица для A\n");
         printf("l. Создать матрицу B\n");
-        printf("m. Сравнить A и B\n");
-        printf("n. Уничтожить A\n");
-        printf("o. Уничтожить B\n");
+        printf("m. Сравнить матрицы A и B\n");
+        printf("n. Уничтожить матрицу A\n");
+        printf("o. Уничтожить матрицу B\n");
         printf("q. Выход\n");
 
-        // Статус матриц
-        printf("Статус: A=%s B=%s\n",
-               matrix_a ? "создана" : "нет",
-               matrix_b ? "создана" : "нет");
+        printf("Состояние: A=%s B=%s\n",
+               is_matrix_created(matrix_a) ? "создана" : "не создана",
+               is_matrix_created(matrix_b) ? "создана" : "не создана");
 
-        printf("Выбор: ");
-        if (scanf(" %c", &choice) == EOF)  // Ctrl+D / Ctrl+Z
+        printf("Выберите действие: ");
+        if (scanf(" %c", &choice) == EOF)
         {
-            printf("Выход\n");
-            if (matrix_a) menu_destroy(&matrix_a);
-            if (matrix_b) menu_destroy(&matrix_b);
+            printf("\nОбнаружен конец ввода (EOF). Завершение программы...\n");
+            printf("До свидания!\n");
             return 0;
         }
-        clear_input();
+        clear();
 
-        // Обработка команд
         switch (choice)
         {
-        case 'a': menu_create_matrix(&matrix_a); break;
-        case 'b': menu_print_matrix(matrix_a); break;
-        case 'c': menu_fill_random(matrix_a); break;
-        case 'd': menu_set_element(matrix_a); break;
-        case 'e': menu_increment(matrix_a); break;
-        case 'f': menu_decrement(matrix_a); break;
-        case 'g': menu_get_row(matrix_a); break;
-        case 'h': menu_get_col(matrix_a); break;
-        case 'i': menu_transpose(&matrix_a); break;
-        case 'j': menu_determinant(matrix_a); break;
-        case 'k': menu_inverse(matrix_a); break;
-        case 'l': menu_create_matrix(&matrix_b); break;
-        case 'm': menu_compare(matrix_a, matrix_b); break;
-        case 'n': menu_destroy(&matrix_a); break;
-        case 'o': menu_destroy(&matrix_b); break;
-        case 'q': printf("До свидания!\n"); break;
-        default: printf("Неверный выбор!\n");
+        case 'a':
+            menu_create_matrix(&matrix_a);
+            break;
+        case 'b':            menu_print_matrix(matrix_a);
+            break;
+        case 'c':
+            menu_fill_random(matrix_a);
+            break;
+        case 'd':
+            menu_set_element(matrix_a);
+            break;
+        case 'e':
+            menu_increment(matrix_a);
+            break;
+        case 'f':
+            menu_decrement(matrix_a);
+            break;
+        case 'g':
+            menu_get_row(matrix_a);
+            break;
+        case 'h':
+            menu_get_col(matrix_a);
+            break;
+        case 'i':
+            menu_transpose(&matrix_a);
+            break;
+        case 'j':
+            menu_determinant(matrix_a);
+            break;
+        case 'k':
+            menu_inverse(matrix_a);
+            break;
+        case 'l':
+            menu_create_matrix(&matrix_b);
+            break;
+        case 'm':
+            menu_compare(matrix_a, matrix_b);
+            break;
+        case 'n':
+            menu_destroy(&matrix_a);
+            break;
+        case 'o':
+            menu_destroy(&matrix_b);
+            break;
+        case 'q':
+            printf("До свидания!\n");
+            break;
+        default:
+            printf("Неверный выбор!\n");
         }
 
         if (choice != 'q')
-        {
-            printf("\nEnter для продолжения...");
+        {            printf("\nНажмите Enter для продолжения...");
             getchar();
         }
 
